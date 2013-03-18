1a0615f9d65c84c389ba79ad166a1d5fcf92060b
Matthew Farrell <farrellm@alum.mit.edu>
2013-03-18 13:37:53 -0400

    indentation

--- src/org/jheme/core/Pair.java
+++ src/org/jheme/core/Pair.java
@@ -5,12 +5,12 @@ import java.util.AbstractSequentialList<E>;
 
 public class Pair<E> extends AbstractSequentialList<E> {
 	private class pair_iterator<E> implements Iterator<E> {
-        private Pair<E> _cur;
-        
-        pair_iterator(Pair<e> p) {
-            _cur = p;
-        }
-        
+		private Pair<E> _cur;
+
+		pair_iterator(Pair<e> p) {
+			_cur = p;
+		}
+
 		@Override
 		public boolean hasNext() {
 			return _cur.cdr != null;
@@ -18,12 +18,12 @@ public class Pair<E> extends AbstractSequentialList<E> {
 
 		@Override
 		public E next() {
-            final E v = _cur.car;
-            _cur = _cur.cdr;
+			final E v = _cur.car;
+			_cur = _cur.cdr;
 			return v;
 		}
 	}
-    
+
 	final private E _car;
 	final private Pair<E> _cdr;
 
